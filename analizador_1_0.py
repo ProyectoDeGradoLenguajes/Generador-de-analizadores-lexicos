51443be07d40653a99e0b20649b0c45816e517db
Sebastian <jspardoj@unal.edu.co>
2016-09-09 11:13:04 -0500

    commit inicial

---
+++ analizador_1_0.py
@@ -0,0 +1,57 @@
+import sys
+import graphviz as graph
+
+"""
+ Crea conexiones para grafos unidireccionales
+"""
+def make_link(G, node1, node2):
+    if node1 not in G:
+        G[node1] = {}
+    (G[node1])[node2] = 1
+    return G
+
+"""
+Recibe el arbol de significado y el nombre que tendra el archivo
+Utiliza la libreria graphviz para pintar el arbol de significado
+"""
+def dibujarArbol(G,etiqueta):
+    g2 = graph.Digraph(format='png')
+    for node1 in G:
+        for node2 in G[node1]:
+            g2.node(str(node1))
+            g2.node(str(node2))
+            g2.edge(str(node1),str(node2))
+    filename = g2.render(filename='img/g'+ etiqueta)
+
+"""
+Recibe una cadena que contiene la definicion del automata y una lista con la posicion de los parentesis
+Retorna el arbol de significado
+
+"""
+def hacerArbol(automata):
+    arbol = {}
+    i = 0
+    while i < len(automata): 
+        
+        i += 1
+    return arbol
+    
+"""
+Lee el archivo de entrada y delega el procesamiento por fases.
+    1) Crear el arbol de significado
+    2) Dibuja el arbol de significado
+    4) Recorre el arbol de significado
+"""
+def programa():
+    numAutomatas = int(sys.stdin.readline().strip())
+    for i in range(numAutomatas):
+        ##print "caso --->", i + 1
+        #Encierra todo el automata en un parentesis para garantizar que sea operado
+        automata = sys.stdin.readline().strip()
+        automata = list(map(str, automata))
+        # automata almacenara una lista con el orden en que deben ser operados los parentesis
+        arbolFinal = hacerArbol(automata)
+        print (arbolFinal)
+        dibujarArbol(arbolFinal, "finals")
+
+programa()
