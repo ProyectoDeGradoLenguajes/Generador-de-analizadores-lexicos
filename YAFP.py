770f7b3d9c0c7b8ab8a73d92e304b43a0e6ac567
jaquinonesg <jaquinonesg@unal.edu.co>
2017-01-17 23:44:05 -0500

    Added the central file where all the action takes place, some basic functionality en fileManagement.py in package folder

---
+++ YAFP.py
@@ -0,0 +1,12 @@
+"""
+Main of the proyect
+All the creation of objects and his interaction should be created here
+Other files contains modules in their respective folders
+"""
+from package import fileManagement as fm
+
+def YAFP():
+    fm.readStructure('input.txt', 'output.txt', '(⌐■_■)')
+
+YAFP()
+    
